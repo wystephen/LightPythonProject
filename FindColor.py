303faa564889dfd3053a290d383ad3bc3b708df6
wang stephen <551619855@qq.com>
2018-02-24 23:25:53 +0800

    s

--- FindColor.py
+++ FindColor.py
@@ -1,2 +1,38 @@
 # -*- coding:utf-8 -*-
 # carete by steve at  2018 / 02 / 24　23:03
+import cv2
+import numpy as np
+import scipy as sp
+
+import matplotlib.pyplot as plt
+
+import os
+
+import PowerTowerDetector
+
+if __name__ == '__main__':
+
+    detector_list = list()
+    ti = 0
+
+    image_list = list()
+    # load all image in the dataset
+    for name in os.listdir('./image'):
+        ti+=1
+        if not ti is 5:
+            continue
+        print(name)
+        t_name = 'image\\' + name
+        im = cv2.imread(t_name)
+        image_list.append(im)
+
+    print('after load image')
+
+    plt.figure(1)
+    for i in range(len(image_list)):
+        img = image_list[i]
+        plt.subplot(2,3,i+1)
+        plt.imshow(img)
+
+
+    plt.show()
--- skimage_test.py
+++ skimage_test.py
@@ -1,25 +1,29 @@
 # -*- coding:utf-8 -*-
 # carete by steve at  2018 / 02 / 24　22:50
-from skimage import data, segmentation, color
-from skimage.future import graph
-from matplotlib import pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
+from skimage.data import camera
+from skimage.filters import roberts, sobel, scharr, prewitt
 import cv2
 
-img = cv2.imread('./image/1.jpg')
+image = cv2.cvtColor(cv2.cvtColor(cv2.imread('image/1.jpg'),
+                                  cv2.COLOR_RGB2HSV),
+                     cv2.COLOR_RGB2GRAY)
+edge_roberts = roberts(image)
+edge_sobel = sobel(image)
 
-labels1 = segmentation.slic(img, compactness=30, n_segments=400)
-out1 = color.label2rgb(labels1, img, kind='avg')
+fig, ax = plt.subplots(ncols=2, sharex=True, sharey=True,
+                       figsize=(8, 4))
 
-g = graph.rag_mean_color(img, labels1, mode='similarity')
-labels2 = graph.cut_normalized(labels1, g)
-out2 = color.label2rgb(labels2, img, kind='avg')
+ax[0].imshow(edge_roberts, cmap=plt.cm.gray)
+ax[0].set_title('Roberts Edge Detection')
 
-fig, ax = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(6, 8))
-
-ax[0].imshow(out1)
-ax[1].imshow(out2)
+ax[1].imshow(edge_sobel, cmap=plt.cm.gray)
+ax[1].set_title('Sobel Edge Detection')
 
 for a in ax:
     a.axis('off')
+
+plt.tight_layout()
 plt.show()
