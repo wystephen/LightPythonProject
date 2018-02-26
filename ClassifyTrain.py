87cf2fa8ed3707b3d0dab2ba611fae37a5086c04
wang stephen <551619855@qq.com>
2018-02-26 12:15:18 +0800

    s

--- ClassifyTrain.py
+++ ClassifyTrain.py
@@ -1,2 +1,11 @@
 # -*- coding:utf-8 -*-
 # carete by steve at  2018 / 02 / 26　11:58
+import numpy as np
+import scipy as sp
+
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    x = np.loadtxt('data_x.csv', delimiter=',')
+    y = np.loadtxt('data_y.csv',delimiter=',')
